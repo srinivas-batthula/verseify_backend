b77962cbaaa84ccc45a0031b7c489033d24c716f
Srinivas Batthula <srinivasbatthula05.official@gmail.com>
2025-04-19 14:25:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,13 +37,13 @@ app.use(cors(corsOptions))
 
 app.options('*', cors(corsOptions))
 
-// const limiter = rateLimit({                                  //Must to be used in production to prevent attacks...
-//     windowMs: 2 * 60 * 1000, // 1 minute
-//     max: 20, // limit each IP to 20 requests per windowMs
-//     message: 'Too many requests from this IP, please try again after 2 minutes',
-//     headers: true,
-// })
-// app.use(limiter)
+const limiter = rateLimit({                                  //Must to be used in production to prevent attacks...
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 15, // limit each IP to 20 requests per windowMs
+    message: 'Too many requests from this IP, please try again after 2 minutes',
+    headers: true,
+})
+app.use(limiter)
 
 app.use(helmet())
 
